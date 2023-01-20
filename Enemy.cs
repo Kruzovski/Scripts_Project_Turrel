06bbec81371b0529e920f42691fbc00ef0d91e6e
Kruzovski <93711099+Kruzovski@users.noreply.github.com>
2023-01-21 04:24:02 +0700

    Update Enemy.cs

--- Enemy.cs
+++ Enemy.cs
@@ -7,17 +7,6 @@ public class Enemy : MonoBehaviour
     public static int health;
     [SerializeField] public int HP;
     [SerializeField] public GameObject Character;
-    [SerializeField] public GameObject Head;
-    [SerializeField] public GameObject BodyPrefab;
-    [SerializeField] public GameObject ButtPrefab;
-    [SerializeField] public GameObject LeftArm1Prefab;
-    [SerializeField] public GameObject LeftArm2Prefab;
-    [SerializeField] public GameObject RightArm1Prefab;
-    [SerializeField] public GameObject RightArm2Prefab;
-    [SerializeField] public GameObject LeftLeg1Prefab;
-    [SerializeField] public GameObject LeftLeg2Prefab;
-    [SerializeField] public GameObject RightLeg1Prefab;
-    [SerializeField] public GameObject RightLeg2Prefab;
 
     private void OnTriggerEnter(Collider collision)
     {
@@ -40,53 +29,13 @@ public class Enemy : MonoBehaviour
     {
 
         Character.GetComponent<Move>().Speed = 0;
-
         Character.GetComponent<BoxCollider>().enabled = false;
 
-        Head.GetComponent<Rigidbody>().useGravity = true;
-        Head.GetComponent<BoxCollider>().isTrigger = false;
-        Head.transform.gameObject.tag = "Obstacle";
-
-        BodyPrefab.GetComponent<Rigidbody>().useGravity = true;
-        BodyPrefab.GetComponent<BoxCollider>().isTrigger = false;
-        BodyPrefab.transform.gameObject.tag = "Obstacle";
-
-        ButtPrefab.GetComponent<Rigidbody>().useGravity = true;
-        ButtPrefab.GetComponent<BoxCollider>().isTrigger = false;
-        ButtPrefab.transform.gameObject.tag = "Obstacle";
-
-        LeftArm1Prefab.GetComponent<Rigidbody>().useGravity = true;
-        LeftArm1Prefab.GetComponent<BoxCollider>().isTrigger = false;
-        LeftArm1Prefab.transform.gameObject.tag = "Obstacle";
-
-        LeftArm2Prefab.GetComponent<Rigidbody>().useGravity = true;
-        LeftArm2Prefab.GetComponent<BoxCollider>().isTrigger = false;
-        LeftArm2Prefab.transform.gameObject.tag = "Obstacle";
-
-        RightArm1Prefab.GetComponent<Rigidbody>().useGravity = true;
-        RightArm1Prefab.GetComponent<BoxCollider>().isTrigger = false;
-        RightArm1Prefab.transform.gameObject.tag = "Obstacle";
-
-        RightArm2Prefab.GetComponent<Rigidbody>().useGravity = true;
-        RightArm2Prefab.GetComponent<BoxCollider>().isTrigger = false;
-        RightArm2Prefab.transform.gameObject.tag = "Obstacle";
-
-        LeftLeg1Prefab.GetComponent<Rigidbody>().useGravity = true;
-        LeftLeg1Prefab.GetComponent<BoxCollider>().isTrigger = false;
-        LeftLeg1Prefab.transform.gameObject.tag = "Obstacle";
-
-        LeftLeg2Prefab.GetComponent<Rigidbody>().useGravity = true;
-        LeftLeg2Prefab.GetComponent<BoxCollider>().isTrigger = false;
-        LeftLeg2Prefab.transform.gameObject.tag = "Obstacle";
-
-        RightLeg1Prefab.GetComponent<Rigidbody>().useGravity = true;
-        RightLeg1Prefab.GetComponent<BoxCollider>().isTrigger = false;
-        RightLeg1Prefab.transform.gameObject.tag = "Obstacle";
-
-        RightLeg2Prefab.GetComponent<Rigidbody>().useGravity = true;
-        RightLeg2Prefab.GetComponent<BoxCollider>().isTrigger = false;
-        RightLeg2Prefab.transform.gameObject.tag = "Obstacle";
-
-
+        foreach (Transform child in transform)
+        {
+            child.GetComponent<Rigidbody>().useGravity = true;
+            child.GetComponent<BoxCollider>().isTrigger = false;
+            child.transform.gameObject.tag = "Obstacle";
+        }
     }
 }
