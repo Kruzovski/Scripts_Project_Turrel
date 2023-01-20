83cc9c9a923a401fdd1c8baa8b22ff51d4d9f697
Kruzovski <93711099+Kruzovski@users.noreply.github.com>
2023-01-20 15:05:38 +0700

    Update MyJoystick.cs

--- MyJoystick.cs
+++ MyJoystick.cs
@@ -10,14 +10,11 @@ public class MyJoystick : MyJoysticSettings
     {
         if (_inputVector.x != 0 || _inputVector.y != 0)
         {
-            //RotateTurrel.MoveCharacter(new Vector3(_inputVector.x, -_inputVector.y, 1));
             RotateTurrel.RotateCharacter(new Vector3(-_inputVector.x, _inputVector.y, -1));
         }
         else
         {
-            //RotateTurrel.MoveCharacter(new Vector3(Input.GetAxis("Horizontal"), 0, (Input.GetAxis("Vertical"))));
             RotateTurrel.RotateCharacter(new Vector3(Input.GetAxis("Horizontal"), 0, (Input.GetAxis("Vertical"))));
-
         }
     }
 }
